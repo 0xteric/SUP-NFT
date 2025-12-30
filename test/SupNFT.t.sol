0f6ba422d09c8b69c8d99d28d619a73f6fb3ca14
mermajax <mermajax@gmail.com>
2025-12-30 20:29:01 +0100

    Burn test

--- test/SupNFT.t.sol
+++ test/SupNFT.t.sol
@@ -6,8 +6,8 @@ import {SupNFT} from "../src/SupNFT.sol";
 
 contract SupNftTest is Test {
     SupNFT public supNFT;
+    address public owner = vm.addr(33);
     address public user = vm.addr(1);
-    address public owner = vm.addr(2);
 
     function setUp() public {
         vm.prank(owner);
@@ -20,13 +20,48 @@ contract SupNftTest is Test {
 
         uint amount = 5;
         uint mintPrice = supNFT.mintPrice();
+        uint userBalanceBefore = user.balance;
+        uint supBalanceBefore = address(supNFT).balance;
 
         supNFT.mint{value: amount * mintPrice}(amount);
 
+        uint userBalanceAfter = user.balance;
+        uint supBalanceAfter = address(supNFT).balance;
+
+        assertTrue(userBalanceBefore > userBalanceAfter);
+        assertEq(supBalanceBefore, supBalanceAfter - amount * mintPrice);
         assertEq(supNFT.totalSupply(), 5);
         assertEq(supNFT.ownerOf(1), user);
         assertEq(supNFT.ownerOf(5), user);
 
         vm.stopPrank();
+    }
+
+    function testBurn() public {
+        vm.deal(user, 1 ether);
+        vm.startPrank(user);
+
+        uint amountToMint = 2;
+        uint amountToBurn = 1;
+
+        uint mintPrice = supNFT.mintPrice();
+        uint userBalanceBefore = user.balance;
+
+        supNFT.mint{value: amountToMint * mintPrice}(amountToMint);
+
+        uint userBalanceMiddle = user.balance;
+
+        supNFT.burn(amountToBurn);
+
+        uint userBalanceAfter = user.balance;
+
+        assertEq(
+            userBalanceBefore,
+            userBalanceMiddle + amountToMint * mintPrice
+        );
+        assertEq(
+            userBalanceAfter,
+            userBalanceMiddle + amountToBurn * mintPrice
+        );
     }
 }
